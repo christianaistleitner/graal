220c19e1978958e0e9ec69d1baa4ab07cf43a2cb
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-04-07 13:12:27 +0200

    Add timers

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
@@ -478,7 +478,17 @@ public final class GCImpl implements GC {
             }
 
             if (!incremental) {
-                HeapImpl.getHeapImpl().getOldGeneration().sweep();
+                Timer tenuredPlanningTimer = timers.tenuredPlanning.open();
+                try {
+                    startTicks = JfrGCEvents.startGCPhasePause();
+                    try {
+                        HeapImpl.getHeapImpl().getOldGeneration().sweep();
+                    } finally {
+                        JfrGCEvents.emitGCPhasePauseEvent(getCollectionEpoch(), "Tenured Planning", startTicks);
+                    }
+                } finally {
+                    tenuredPlanningTimer.close();
+                }
             }
 
             Timer referenceObjectsTimer = timers.referenceObjects.open();
@@ -495,7 +505,17 @@ public final class GCImpl implements GC {
             }
 
             if (!incremental) {
-                HeapImpl.getHeapImpl().getOldGeneration().compact();
+                Timer tenuredCompactingTimer = timers.tenuredCompacting.open();
+                try {
+                    startTicks = JfrGCEvents.startGCPhasePause();
+                    try {
+                        HeapImpl.getHeapImpl().getOldGeneration().compact();
+                    } finally {
+                        JfrGCEvents.emitGCPhasePauseEvent(getCollectionEpoch(), "Tenured Compacting", startTicks);
+                    }
+                } finally {
+                    tenuredCompactingTimer.close();
+                }
             }
 
             if (RuntimeCompilation.isEnabled()) {
