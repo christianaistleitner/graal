6545397a255f7c49e067e64825fee3fb4e39fdb4
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-04-13 08:16:22 +0200

    Split compaction and fixing phase

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
@@ -488,7 +488,7 @@ public final class GCImpl implements GC {
                 try {
                     startTicks = JfrGCEvents.startGCPhasePause();
                     try {
-                        HeapImpl.getHeapImpl().getOldGeneration().sweep();
+                        HeapImpl.getHeapImpl().getOldGeneration().planning();
                     } finally {
                         JfrGCEvents.emitGCPhasePauseEvent(getCollectionEpoch(), "Tenured Planning", startTicks);
                     }
@@ -511,6 +511,18 @@ public final class GCImpl implements GC {
             }
 
             if (!incremental) {
+                Timer tenuredFixingTimer = timers.tenuredFixing.open();
+                try {
+                    startTicks = JfrGCEvents.startGCPhasePause();
+                    try {
+                        HeapImpl.getHeapImpl().getOldGeneration().fixing();
+                    } finally {
+                        JfrGCEvents.emitGCPhasePauseEvent(getCollectionEpoch(), "Tenured Fixing", startTicks);
+                    }
+                } finally {
+                    tenuredFixingTimer.close();
+                }
+
                 Timer tenuredCompactingTimer = timers.tenuredCompacting.open();
                 try {
                     startTicks = JfrGCEvents.startGCPhasePause();
