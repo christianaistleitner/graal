2d01ae68a5c322b60ee396c76f8e91db953db10f
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-04-13 09:39:43 +0200

    Optimize compaction code

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
@@ -517,7 +517,7 @@ public final class GCImpl implements GC {
                 try {
                     startTicks = JfrGCEvents.startGCPhasePause();
                     try {
-                        HeapImpl.getHeapImpl().getOldGeneration().compact();
+                        HeapImpl.getHeapImpl().getOldGeneration().compacting();
                     } finally {
                         JfrGCEvents.emitGCPhasePauseEvent(getCollectionEpoch(), "Tenured Compacting", startTicks);
                     }
