58a42d9772bf2869310a19f2268cc4803e8d7bce
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-04-21 09:19:43 +0200

    Ignore optional identity hash field when doing compacting (for now)

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/tenured/PlanningVisitor.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/tenured/PlanningVisitor.java
@@ -2,7 +2,6 @@ package com.oracle.svm.core.genscavenge.tenured;
 
 import com.oracle.svm.core.AlwaysInline;
 import com.oracle.svm.core.NeverInline;
-import com.oracle.svm.core.config.ConfigurationValues;
 import com.oracle.svm.core.genscavenge.AlignedHeapChunk;
 import com.oracle.svm.core.genscavenge.HeapChunk;
 import com.oracle.svm.core.genscavenge.ObjectHeaderImpl;
@@ -13,9 +12,6 @@ import org.graalvm.nativeimage.Platforms;
 import org.graalvm.word.Pointer;
 import org.graalvm.word.UnsignedWord;
 import org.graalvm.word.WordFactory;
-
-import static org.graalvm.compiler.nodes.extended.BranchProbabilityNode.SLOW_PATH_PROBABILITY;
-import static org.graalvm.compiler.nodes.extended.BranchProbabilityNode.probability;
 
 public class PlanningVisitor implements AlignedHeapChunk.Visitor {
 
@@ -40,7 +36,15 @@ public class PlanningVisitor implements AlignedHeapChunk.Visitor {
         UnsignedWord gapSize = WordFactory.zero();
 
         while (cursor.belowThan(top)) {
+            Word header = ObjectHeaderImpl.readHeaderFromPointer(cursor);
             Object obj = cursor.toObject();
+
+            /*
+             * Adding the optional identity hash field will increase the object's size,
+             * but in here, when compacting the tenured space, we expect that there aren't any marked objects
+             * which have their "IdentityHashFromAddress" object header flag set.
+             */
+            assert !ObjectHeaderImpl.hasIdentityHashFromAddressInline(header);
             UnsignedWord objSize = LayoutEncoding.getSizeFromObjectInlineInGC(obj);
 
             if (ObjectHeaderImpl.hasMarkedBit(obj)) {
@@ -67,16 +71,6 @@ public class PlanningVisitor implements AlignedHeapChunk.Visitor {
                     RelocationInfo.writeNextPlugOffset(relocationInfoPointer, 0);
 
                     gapSize = WordFactory.zero();
-                }
-
-                /*
-                 * Adding the optional identity hash field will increase the object's size.
-                 */
-                if (!ConfigurationValues.getObjectLayout().hasFixedIdentityHashField()) {
-                    Word header = ObjectHeaderImpl.readHeaderFromObject(obj);
-                    if (probability(SLOW_PATH_PROBABILITY, ObjectHeaderImpl.hasIdentityHashFromAddressInline(header))) {
-                        objSize = LayoutEncoding.getSizeFromObjectInlineInGC(obj, true);
-                    }
                 }
 
                 relocationPointer = relocationPointer.add(objSize);
