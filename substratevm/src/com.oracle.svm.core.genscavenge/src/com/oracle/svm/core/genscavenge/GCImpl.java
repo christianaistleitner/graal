ffc7c9d84a88c4f443797fe5b2e408c816ae33dc
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-06-15 09:57:35 +0200

    Cleanup and fixed ref fixing bug in unaligned chunks

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/GCImpl.java
@@ -521,6 +521,11 @@ public final class GCImpl implements GC {
         GreyToBlackObjRefVisitor.Counters counters = greyToBlackObjRefVisitor.openCounters();
         long startTicks;
         try {
+            if (!incremental) {
+                HeapImpl heap = HeapImpl.getHeapImpl();
+                heap.getOldGeneration().absorb(heap.getYoungGeneration());
+            }
+
             Timer rootScanTimer = timers.rootScan.open();
             try {
                 startTicks = JfrGCEvents.startGCPhasePause();
@@ -545,19 +550,6 @@ public final class GCImpl implements GC {
                 } finally {
                     tenuredPlanningTimer.close();
                 }
-            }
-
-            Timer referenceObjectsTimer = timers.referenceObjects.open();
-            try {
-                startTicks = JfrGCEvents.startGCPhasePause();
-                try {
-                    Reference<?> newlyPendingList = ReferenceObjectProcessing.processRememberedReferences();
-                    HeapImpl.getHeapImpl().addToReferencePendingList(newlyPendingList);
-                } finally {
-                    JfrGCEvents.emitGCPhasePauseEvent(getCollectionEpoch(), "Process Remembered References", startTicks);
-                }
-            } finally {
-                referenceObjectsTimer.close();
             }
 
             if (!incremental) {
@@ -586,6 +578,19 @@ public final class GCImpl implements GC {
                 }
             }
 
+            Timer referenceObjectsTimer = timers.referenceObjects.open();
+            try {
+                startTicks = JfrGCEvents.startGCPhasePause();
+                try {
+                    Reference<?> newlyPendingList = ReferenceObjectProcessing.processRememberedReferences();
+                    HeapImpl.getHeapImpl().addToReferencePendingList(newlyPendingList);
+                } finally {
+                    JfrGCEvents.emitGCPhasePauseEvent(getCollectionEpoch(), "Process Remembered References", startTicks);
+                }
+            } finally {
+                referenceObjectsTimer.close();
+            }
+
             if (RuntimeCompilation.isEnabled()) {
                 Timer cleanCodeCacheTimer = timers.cleanCodeCache.open();
                 try {
@@ -607,7 +612,7 @@ public final class GCImpl implements GC {
 
             Timer releaseSpacesTimer = timers.releaseSpaces.open();
             try {
-                assert chunkReleaser.isEmpty();
+                // assert chunkReleaser.isEmpty(); TODO: chunkReleaser is not empty as we already added unaligned chunks
                 startTicks = JfrGCEvents.startGCPhasePause();
                 try {
                     releaseSpaces();
@@ -1126,10 +1131,11 @@ public final class GCImpl implements GC {
         Header<?> originalChunk = getChunk(original, isAligned);
         Space originalSpace = HeapChunk.getSpace(originalChunk);
 
-        if (originalSpace.isOldSpace()) {
+        if (completeCollection) {
             // Mark objects in the old generation and continue depth-first traversal
             // as referenced objects aren't colored gray by copying!
             ObjectHeaderImpl.setMarkedBit(original);
+            ObjectHeaderImpl.setRememberedSetBit(original);
             // TODO: This recursive call will cause stack overflows. The Deutsch-Schorr-Waite algorithm would fix that.
             greyToBlackObjectVisitor.visitObject(original);
             return original; // Objects in the old generation cannot be promoted further.
@@ -1188,7 +1194,6 @@ public final class GCImpl implements GC {
                     }
                 }
                 if (!promoted) {
-                    // Promote the whole chunk as fallback.
                     heap.getOldGeneration().promoteChunk(originalChunk, isAligned, originalSpace);
                 }
             } else if (originalSpace.isOldSpace()) {
