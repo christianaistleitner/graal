d677a9254405a1df484735b8a2283e47e8af96ae
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-06-16 08:55:52 +0200

    Disable added log outputs

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/OldGeneration.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/OldGeneration.java
@@ -158,7 +158,7 @@ public final class OldGeneration extends Generation {
         timers.tenuredFixingAlignedChunks.open();
         AlignedHeapChunk.AlignedHeader aChunk = space.getFirstAlignedHeapChunk();
         while (aChunk.isNonNull()) {
-            Log.log().string("[OldGeneration.fixing: fixing phase, chunk=").zhex(aChunk)
+            Log.noopLog().string("[OldGeneration.fixing: fixing phase, chunk=").zhex(aChunk)
                     .string(", top=").zhex(HeapChunk.getTopPointer(aChunk))
                     .string("]").newline().flush();
             RelocationInfo.walkObjects(aChunk, fixingVisitor);
@@ -200,19 +200,16 @@ public final class OldGeneration extends Generation {
          */
         timers.tenuredFixingUnalignedChunks.open();
         try {
-            Log.log().string("A\n").flush();
             UnalignedHeapChunk.UnalignedHeader uChunk = space.getFirstUnalignedHeapChunk();
             while (uChunk.isNonNull()) {
                 UnalignedHeapChunk.UnalignedHeader next = HeapChunk.getNext(uChunk);
-                Log.log().string("B\n").flush();
                 Pointer objPointer = UnalignedHeapChunk.getObjectStart(uChunk);
                 Object obj = objPointer.toObject();
                 if (ObjectHeaderImpl.hasMarkedBit(obj)) {
-                    Log.log().string("C\n").flush();
                     ObjectHeaderImpl.clearMarkedBit(obj);
                     RememberedSet.get().clearRememberedSet(uChunk);
 
-                    Log.log().string("[OldGeneration.fixing: fixing phase, chunk=").zhex(uChunk)
+                    Log.noopLog().string("[OldGeneration.fixing: fixing phase, chunk=").zhex(uChunk)
                             .string(", unaligned]").newline().flush();
 
                     UnalignedHeapChunk.walkObjects(uChunk, fixingVisitor);
@@ -220,7 +217,6 @@ public final class OldGeneration extends Generation {
                     UnsignedWord objSize = LayoutEncoding.getSizeFromObjectInlineInGC(obj);
                     assert UnalignedHeapChunk.getObjectStart(uChunk).add(objSize).equal(HeapChunk.getTopPointer(uChunk));
                 } else {
-                    Log.log().string("D, ").zhex(uChunk).newline().flush();
                     space.extractUnalignedHeapChunk(uChunk);
                     chunkReleaser.add(uChunk);
                 }
@@ -236,7 +232,7 @@ public final class OldGeneration extends Generation {
         timers.tenuredCompactingCunks.open();
         AlignedHeapChunk.AlignedHeader chunk = space.getFirstAlignedHeapChunk();
         while (chunk.isNonNull()) {
-            Log.log().string("[OldGeneration.compacting: chunk=").zhex(chunk)
+            Log.noopLog().string("[OldGeneration.compacting: chunk=").zhex(chunk)
                     .string("]\n").flush();
 
             compactingVisitor.init(chunk);
@@ -249,7 +245,7 @@ public final class OldGeneration extends Generation {
         chunk = space.getFirstAlignedHeapChunk();
         timers.tenuredUpdatingRememberedSet.open();
         while (chunk.isNonNull()) {
-            Log.log().string("[OldGeneration.compacting: chunk=").zhex(chunk)
+            Log.noopLog().string("[OldGeneration.compacting: chunk=").zhex(chunk)
                     .string(", top=").zhex(HeapChunk.getTopPointer(chunk))
                     .string(", done]\n").flush();
 
@@ -271,7 +267,7 @@ public final class OldGeneration extends Generation {
                 // Release the empty aligned chunk.
                 space.extractAlignedHeapChunk(aChunk);
                 chunkReleaser.add(aChunk);
-                Log.log().string("[OldGeneration.releaseSpaces: chunk=").zhex(aChunk).newline().flush();
+                Log.noopLog().string("[OldGeneration.releaseSpaces: chunk=").zhex(aChunk).newline().flush();
             }
             aChunk = next;
         }
