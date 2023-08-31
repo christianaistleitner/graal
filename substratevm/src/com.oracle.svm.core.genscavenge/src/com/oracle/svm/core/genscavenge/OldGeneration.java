130cefecbcbdbe4adb73b5ddee13867767b39461
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-08-31 10:43:03 +0200

    [GR-18214] Sweep on low fragmention

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/OldGeneration.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/OldGeneration.java
@@ -37,6 +37,7 @@ import com.oracle.svm.core.genscavenge.tenured.FixingVisitor;
 import com.oracle.svm.core.genscavenge.tenured.PlanningVisitor;
 import com.oracle.svm.core.genscavenge.tenured.RefFixingVisitor;
 import com.oracle.svm.core.genscavenge.tenured.RelocationInfo;
+import com.oracle.svm.core.genscavenge.tenured.SweepingVisitor;
 import com.oracle.svm.core.graal.RuntimeCompilation;
 import com.oracle.svm.core.heap.ObjectVisitor;
 import com.oracle.svm.core.hub.LayoutEncoding;
@@ -70,6 +71,7 @@ public final class OldGeneration extends Generation {
     private final RefFixingVisitor refFixingVisitor = new RefFixingVisitor();
     private final FixingVisitor fixingVisitor = new FixingVisitor(refFixingVisitor);
     private final CompactingVisitor compactingVisitor = new CompactingVisitor();
+    private final SweepingVisitor sweepingVisitor = new SweepingVisitor();
     private final RuntimeCodeCacheWalker runtimeCodeCacheWalker = new RuntimeCodeCacheWalker(refFixingVisitor);
 
     @Platforms(Platform.HOSTED_ONLY.class)
@@ -255,8 +257,13 @@ public final class OldGeneration extends Generation {
             Log.noopLog().string("[OldGeneration.compacting: chunk=").zhex(chunk)
                     .string("]\n").flush();
 
-            compactingVisitor.init(chunk);
-            RelocationInfo.walkObjects(chunk, compactingVisitor);
+            if (chunk.getShouldSweepInsteadOfCompact()) {
+                RelocationInfo.visit(chunk, sweepingVisitor);
+                chunk.setShouldSweepInsteadOfCompact(false);
+            } else {
+                compactingVisitor.init(chunk);
+                RelocationInfo.visit(chunk, compactingVisitor);
+            }
 
             chunk = HeapChunk.getNext(chunk);
         }
