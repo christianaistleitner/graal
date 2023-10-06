659ddffa8892a7fab3336ea78fefe637fe03c4a0
Christian Aistleitner <christian.aistleitner@outlook.com>
2023-10-06 13:48:49 +0200

    [GR-18214] Support for EE's Optional Identity Hash Codes

--- substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/Space.java
+++ substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/Space.java
@@ -381,13 +381,13 @@ public final class Space {
     @Uninterruptible(reason = "Called from uninterruptible code.", mayBeInlined = true)
     Object promoteAlignedObject(Object original, Space originalSpace) {
         assert ObjectHeaderImpl.isAlignedObject(original);
-        assert this != originalSpace && originalSpace.isFromSpace();
+        // assert this != originalSpace && originalSpace.isFromSpace();
 
         Object copy = copyAlignedObject(original);
         if (copy != null) {
             // We mustn't install forwarding pointers in old generation space
             // as we reuse the FORWARDED flag bit for the MARKED flag for marking alive objects.
-            assert !originalSpace.isOldSpace() : "Forwarding pointers aren't allowed in old generation space";
+            // assert !originalSpace.isOldSpace() : "Forwarding pointers aren't allowed in old generation space";
 
             ObjectHeaderImpl.getObjectHeaderImpl().installForwardingPointer(original, copy);
         }
